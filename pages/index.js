01e1cbb49cf2db105740a339bc93aded1a23c38f
DaveRoRa <ftdrodriguezr@gmail.com>
2022-07-01 17:59:12 -0400

    Article Layout

--- pages/index.js
+++ pages/index.js
@@ -3,35 +3,38 @@ import { Container, Box, Heading } from '@chakra-ui/react';
 import ProfilePic from '../components/ProfilePic';
 import Section, { Paragraph } from '../components/Section';
 import BioSection from '../components/BioSection';
+import Article from '../components/layout/Article';
 
 function MainPage() {
   return (
-    <Container>
-      <Box borderRadius='lg' bg='red' mb={6} p={3} alignItems='center'>
-        Presentación personal
-      </Box>
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
-          <Heading as='h2' variant='page-tile'>
-            David Rodriguez Ramirez
-          </Heading>
-          <p>Descripción &quot;artística&quot;</p>
+    <Article>
+      <Container>
+        <Box borderRadius='lg' bg='whiteAlpha.200' mb={6} p={3} alignItems='center'>
+          Presentación personal
         </Box>
-        <ProfilePic />
-      </Box>
-      <Section delay={0.1}>
-        <Heading as='h3' variant='section-title'>
-          Work
-        </Heading>
-        <Paragraph>Paragraph</Paragraph>
-      </Section>
-      <Section delay={0.2}>
-        <Heading as='h3' variant='section-title'>
-          Bio
-        </Heading>
-        <BioSection />
-      </Section>
-    </Container>
+        <Box display={{ md: 'flex' }}>
+          <Box flexGrow={1}>
+            <Heading as='h2' variant='page-tile'>
+              David Rodriguez Ramirez
+            </Heading>
+            <p>Descripción &quot;artística&quot;</p>
+          </Box>
+          <ProfilePic />
+        </Box>
+        <Section delay={0.1}>
+          <Heading as='h3' variant='section-title'>
+            Work
+          </Heading>
+          <Paragraph>Paragraph</Paragraph>
+        </Section>
+        <Section delay={0.2}>
+          <Heading as='h3' variant='section-title'>
+            Bio
+          </Heading>
+          <BioSection />
+        </Section>
+      </Container>
+    </Article>
   );
 }
 
--- pages/works.js
+++ pages/works.js
@@ -1,33 +1,36 @@
 import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react';
 import { WorkGridItem } from '../components/GridItem';
 import Section from '../components/Section';
+import Article from '../components/layout/Article';
 
 const inkTumb = '/images/pokemon-scarlet-violet-pawmi.webp';
 
 const WorksPage = () => (
-  <Container>
-    <Heading as='h3' align='center' fontSize={20} mb={4}>
-      Works
-    </Heading>
-    <SimpleGrid columns={[1, null, null, 2]} gap={6}>
-      <Section>
-        <WorkGridItem id='inkdrop' title='Inkdrop' thumbnail={inkTumb}>
-          A markdown note-taking app
-        </WorkGridItem>
-      </Section>
-      <Section>
-        <WorkGridItem id='inkdrop' title='Inkdrop' thumbnail={inkTumb}>
-          A markdown note-taking app
-        </WorkGridItem>
-      </Section>
-    </SimpleGrid>
-    <Section delay={0.4}>
-      <Divider my={6} />
+  <Article>
+    <Container>
       <Heading as='h3' align='center' fontSize={20} mb={4}>
-        Old Works
+        Works
       </Heading>
-    </Section>
-  </Container>
+      <SimpleGrid columns={[1, null, null, 2]} gap={6}>
+        <Section>
+          <WorkGridItem id='inkdrop' title='Inkdrop' thumbnail={inkTumb}>
+            A markdown note-taking app
+          </WorkGridItem>
+        </Section>
+        <Section>
+          <WorkGridItem id='inkdrop' title='Inkdrop' thumbnail={inkTumb}>
+            A markdown note-taking app
+          </WorkGridItem>
+        </Section>
+      </SimpleGrid>
+      <Section delay={0.4}>
+        <Divider my={6} />
+        <Heading as='h3' align='center' fontSize={20} mb={4}>
+          Old Works
+        </Heading>
+      </Section>
+    </Container>
+  </Article>
 );
 
 export default WorksPage;
